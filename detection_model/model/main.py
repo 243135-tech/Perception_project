ae9bf4c9c6ebd93f59a6770698f0d3b449a0ef3b
JOAN SAURINA RICÓS <joan.saurina@estudiantat.upc.edu>
2024-12-05 17:31:10 +0100

    'structure'

---
+++ detection_model/model/main.py
@@ -0,0 +1,131 @@
+import argparse
+import os
+from pathlib import Path
+import cv2
+from ultralytics import YOLO
+from matplotlib import pyplot as plt
+from ..src.sort import Sort
+from ..utils.occlusion import *
+from ..src.kalman import *
+from ..src.detection import *
+from ..utils.export_video import *
+from ..utils.distance import *
+
+def main(set_img, output_video_path, frame_rate, confidence_threshold):
+    # Initialize the YOLO model
+    model = YOLO("yolov8l.pt") 
+
+    # Initialize SORT tracker
+    mot_tracker = Sort(max_age=3, min_hits=1, iou_threshold=0.5)
+
+    overlap_image = resize_image("Overlap_image.png", width=217, height=225)
+
+    # Define input and obj folders
+    if set_img == 1:  # first sequence
+        output_folder = "outputs/outputs1"
+        data_folder_1 = "data/view1"
+        data_folder_2 = "data/view2"
+    elif set_img == 2:  # second sequence
+        output_folder = "outputs/outputs2"
+        data_folder_1 = "data/view3"
+        data_folder_2 = "data/view4"
+    else:  # third sequence
+        output_folder = "outputs/outputs3"
+        data_folder_1 = "data/view5"
+        data_folder_2 = "data/view6"
+
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Initialize a list to store occluded predictions
+    tracked_predictions = {}
+    outputs = []
+    labels_dict = {}
+
+    # Main loop for processing each frame
+    for left_frame_path, right_frame_path in zip(
+        sorted(Path(data_folder_1).glob("*.png")), 
+        sorted(Path(data_folder_2).glob("*.png"))
+    ):
+        # Process left and right frames
+        dets, labels, img, ids, lables_left = process_frame(left_frame_path, model, confidence_threshold)
+        dets_right, labels_right, img_right, ids_right, lables_right = process_frame(right_frame_path, model, confidence_threshold)
+
+        distances = calculate_distance(lables_left, lables_right)
+        bearings = get_bearing_from_stereo(lables_left, lables_right)
+        rotations = calculate_rotation_y(lables_left, lables_right)
+        heights = get_height(lables_left, lables_right)
+        widths = get_width(lables_left, lables_right)
+        lengths = get_length(lables_left, lables_right, img, img_right)
+        truncations = get_truncated(lables_left, lables_right, img, img_right)
+        cam_positions = get_camera_position(lables_left, lables_right)
+
+        if img is None:
+            continue
+
+        draw_detected_objects(img, dets, labels, distances)
+        trackers = mot_tracker.update(dets)
+        overlay_rect, img = find_overlay_rectangle(img, overlap_image, set_img, threshold=0.2)
+
+        for i, d in enumerate(trackers):
+            distance, bearing, rotation, height, width, length, truncated, x_cam, y_cam, z_cam = get_parameters(
+                i, distances, bearings, rotations, heights, widths, lengths, truncations, cam_positions
+            )
+            label = labels[i]
+            outputs, tracked_predictions = process_tracked_object(
+                d=d,
+                img=img,
+                overlay_rect=overlay_rect,
+                frame_path=left_frame_path,
+                label=label,
+                outputs=outputs,
+                tracked_predictions=tracked_predictions,
+                distance=distance,
+                bearing=bearing,
+                rotation=rotation,
+                height=height,
+                width=width,
+                length=length,
+                truncated=truncated,
+                x_cam=x_cam,
+                y_cam=y_cam,
+                z_cam=z_cam
+            )
+
+        for track_id, prediction in tracked_predictions.items():
+            outputs, tracked_predictions = process_prediction(
+                track_id=track_id,
+                prediction=prediction,
+                frame_name=left_frame_path,
+                overlay_rect=overlay_rect,
+                img=img,
+                tracked_predictions=tracked_predictions,
+                labels_dict=labels_dict,
+                outputs=outputs
+            )
+
+        output_path = os.path.join(output_folder, left_frame_path.name)
+        cv2.imwrite(output_path, img)
+
+    # Create video from images
+    create_video_from_images(output_folder, output_video_path, frame_rate, preview=False)
+
+    # Save outputs to a text file
+    with open("outputs_text.txt", "w") as file:
+        for output in outputs:
+            file.write(str(output) + "\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process video frames using YOLO and SORT tracker.")
+    parser.add_argument("--set_img", type=int, default=2, help="Sequence number (1, 2, or 3)")
+    parser.add_argument("--output_video_path", type=str, default="clean/video2.mp4", help="Output video path")
+    parser.add_argument("--frame_rate", type=int, default=20, help="Frame rate for the output video")
+    parser.add_argument("--confidence_threshold", type=float, default=0.5, help="Confidence threshold for object detection")
+
+    args = parser.parse_args()
+    main(
+        set_img=args.set_img,
+        output_video_path=args.output_video_path,
+        frame_rate=args.frame_rate,
+        confidence_threshold=args.confidence_threshold
+    )
---
+++ detection_model/src/detection.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+
+def process_frame(frame_path, model, confidence_threshold=0.5, valid_classes=["person", "car", "truck", "pedestrian"]):
+    """
+    Loads an image, performs object detection, and processes detections.
+    """
+
+    # Load the image
+    img = cv2.imread(str(frame_path))
+    ids = []
+    
+    # Check if the image is loaded correctly
+    if img is None:
+        print(f"Error: Could not load image {frame_path.name}")
+        return None, None, None, None
+
+    # Perform object detection
+    results = model(img, conf=confidence_threshold, classes=[0, 1, 2, 7])
+    detections = results[0].boxes
+
+    dets = []
+    labels = []
+    match = []
+
+    # Log processing details
+    print("Processing frame:", frame_path.name)
+    #print("Number of detections:", len(detections))
+
+    # Process each detection
+    for box in detections:
+        x1, y1, x2, y2 = box.xyxy[0].tolist()
+        confidence = float(box.conf[0])
+        cls = int(box.cls[0])
+        label = model.names[cls]
+        labels.append(label)
+        
+
+        # Filter detections based on confidence and valid classes
+        if confidence < confidence_threshold or label not in valid_classes:
+            continue
+
+        # Append valid detection
+        dets.append([x1, y1, x2, y2, confidence])
+        match.append([x1,y1,x2,y2, label])
+
+
+    # Convert detections to NumPy array
+    dets = np.array(dets)
+
+    return dets, labels, img, ids, match
+
+def draw_detected_objects(img, dets, labels, distances):
+    for det, label in zip(dets, labels):
+        x1, y1, x2, y2, confidence = map(int, det)
+        
+        # Find the matching distance for this x1 coordinate
+        matched_distance = "inf"  # Default if no match
+        for dist_entry in distances:
+            if int(dist_entry[0]) == y1:  # Match x1
+                matched_distance = f"{dist_entry[2]:.2f}m"  # Format distance
+                break
+        
+        # Draw the bounding box and label with distance
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Green box
+        label_with_distance = f"{label}: {matched_distance}"
+        cv2.putText(img, label_with_distance, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
